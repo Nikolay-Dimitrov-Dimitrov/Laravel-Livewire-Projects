4989baae398b7718991888b59f10a95fc9cde47f
Nikolay Dimitrov <nikolay.joburg@gmail.com>
2023-04-07 13:45:08 +0200

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Continent;
+use App\Models\Country;
+use App\Models\Product;
+use Database\Factories\CountryFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $continents = [
+            ['id' => 1, 'name' => 'Europe',],
+            ['id' => 2, 'name' => 'Asia',],
+            ['id' => 3, 'name' => 'Africa',],
+            ['id' => 4, 'name' => 'South America',],
+            ['id' => 5, 'name' => 'North America',],
+        ];
+        foreach ($continents as $continent) {
+            Continent::factory()
+                ->has(Country::factory()->count(10))
+                ->create($continent);    
+        }
+
+        Product::factory(100)->create();
+    }
+}
